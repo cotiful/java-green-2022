85463f48f15a7afbfe26ccfe0af9e634727f3d2f
유승현 <yuseunghyeon@yuseunghyeon-ui-MacBookAir.local>
2022-02-07 14:59:00 +0900

    코로나 실습완료

--- src/corona/CoronaDownload.java
+++ src/corona/CoronaDownload.java
@@ -25,12 +25,43 @@ public class CoronaDownload {
             Gson gson = new Gson();
             CoronaDto dto = gson.fromJson(responseJson, CoronaDto.class);
 
-            String resultCode = dto.getResponse().getBody().getItems().getItem().get(5).getDecideCnt();
-            String resultCode_1 = dto.getResponse().getBody().getItems().getItem().get(5).getStateDt();
-            System.out.println(resultCode_1 + "시작일" + "총확진자수=" + resultCode);
-            String resultCode2 = dto.getResponse().getBody().getItems().getItem().get(0).getDecideCnt();
-            String resultCode2_1 = dto.getResponse().getBody().getItems().getItem().get(0).getStateDt();
-            System.out.println(resultCode2_1 + "종료일" + "총확진자수=" + resultCode2);
+            String resultAcc = dto.getResponse().getBody().getItems().getItem().get(5).getAccExamCnt();
+            String resultCre = dto.getResponse().getBody().getItems().getItem().get(5).getCreateDt();
+            String resultDeath = dto.getResponse().getBody().getItems().getItem().get(5).getDeathCnt();
+            String resultDeci = dto.getResponse().getBody().getItems().getItem().get(5).getDecideCnt();
+            String resultSeq = dto.getResponse().getBody().getItems().getItem().get(5).getSeq();
+            String resultStatedt = dto.getResponse().getBody().getItems().getItem().get(5).getStateDt();
+            String resultTime = dto.getResponse().getBody().getItems().getItem().get(5).getStateTime();
+            String resultUpdate = dto.getResponse().getBody().getItems().getItem().get(5).getUpdateDt();
+
+            System.out.println("1월 10일 시작일");
+            System.out.println("누적 의심신고 검사자=" + resultAcc);
+            System.out.println("등록일시분초=" + resultCre);
+            System.out.println("사망자 수=" + resultDeath);
+            System.out.println("확진자 수=" + resultDeci);
+            System.out.println("감염현황 고유값=" + resultSeq);
+            System.out.println("기준일=" + resultStatedt);
+            System.out.println("기준시간=" + resultTime);
+            System.out.println("수정일시분초=" + resultUpdate);
+            String resultAcc1 = dto.getResponse().getBody().getItems().getItem().get(5).getAccExamCnt();
+            String resultCre1 = dto.getResponse().getBody().getItems().getItem().get(5).getCreateDt();
+            String resultDeath1 = dto.getResponse().getBody().getItems().getItem().get(5).getDeathCnt();
+            String resultDeci1 = dto.getResponse().getBody().getItems().getItem().get(5).getDecideCnt();
+            String resultSeq1 = dto.getResponse().getBody().getItems().getItem().get(5).getSeq();
+            String resultStatedt1 = dto.getResponse().getBody().getItems().getItem().get(5).getStateDt();
+            String resultTime1 = dto.getResponse().getBody().getItems().getItem().get(5).getStateTime();
+            String resultUpdate1 = dto.getResponse().getBody().getItems().getItem().get(5).getUpdateDt();
+
+            System.out.println("==============");
+            System.out.println("1월 15일 종료일");
+            System.out.println("누적 의심신고 검사자=" + resultAcc1);
+            System.out.println("등록일시분초=" + resultCre1);
+            System.out.println("사망자 수=" + resultDeath1);
+            System.out.println("확진자 수=" + resultDeci1);
+            System.out.println("감염현황 고유값=" + resultSeq1);
+            System.out.println("기준일=" + resultStatedt1);
+            System.out.println("기준시간=" + resultTime1);
+            System.out.println("수정일시분초=" + resultUpdate1);
 
         } catch (Exception e) {
             e.printStackTrace();
